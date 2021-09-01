293ef972dde458c2586cc9b9f334394113bcfc95
Joseph Wagner <wjoseph0@protonmail.com>
2021-09-01 15:20:31 -0500

    Calculator structure

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(num1, num2) {return num1 + num2}
+
+function subtract(num1, num2) {return num1 - num2}
+
+function multiply(num1, num2) {return num1 * num2}
+
+function divide(num1, num2) {return num1 / num2}
+
+
+function operate(operator, num1, num2) {
+  if (operator === "add") return add(num1, num2);
+  if (operator === "subtract") return subtract(num1, num2);
+  if (operator === "multiply") return multiply(num1, num2);
+  if (operator === "divide") return divide(num1, num2);
+}
